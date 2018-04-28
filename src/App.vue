d6942eb489f45452ddabd507f6428e5130ed7736
ahgCoding <805024102@qq.com>
2018-04-28 14:55:34 +0800

    页面加载进度条{

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <div id="app" class="app">
-    <router-view  v-wechat-title="$route.meta.title" ></router-view>
-  </div>
+    <div id="app" class="app">
+        <router-view v-wechat-title="$route.meta.title"></router-view>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+    import NProgress from 'nprogress'
+    import 'nprogress/nprogress.css'
+    export default {
+        name: 'app',
+        created(){
+            //    路由全局钩子
+            this.$router.beforeEach((to, from, next) => {
+                NProgress.start();
+                next()
+            });
+            this.$router.afterEach((to) => {
+                NProgress.done();
+            });
+
+            let channel = this.$route.query.channel || '';
+            this.global.cookie.set("channel", channel);
+        },
+    }
 </script>
 
 <style lang="less">
-@import '~vux/src/styles/reset.less';
-@import '~vux/src/styles/1px.less';
-@import './assets/css/reset.less';
+    @import '~vux/src/styles/reset.less';
+    @import '~vux/src/styles/1px.less';
+    @import './assets/css/reset.less';
 </style>
